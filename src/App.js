d3df39b4ca6cd4b657d1ca19d6cd4082df83ec5f
shield <shield@localhost.localdomain>
2022-04-30 16:51:30 +0100

    added thanks and email confirm page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router,Route , Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import Register from "./pages/Register";
+import Thanks from "./pages/Thanks";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
           <Routes>
             <Route path="/" element={ <Home /> } /> 
             <Route path="register" element={ <Register /> } /> 
+            <Route path="thanks" element={ <Thanks /> } /> 
 
           </Routes>
         </Router>
---
+++ src/pages/Thanks.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import MainFooter from '../components/MainFooter';
+import MainHeader from '../components/MainHeader';
+
+export default function Thanks() {
+  return (
+    <div className='h-full'>
+        <MainHeader />
+
+
+        <div class="container bg-indigo-50 border-inherit rounded-xl shadow-2xl shadow-indigo-500/50 mx-auto mt-10 mb-20  px-4 py-6 lg:px-20  w-auto">
+             <h1 class="font-bold text-2xl text-gray-700 pb-10 text-center">Your Account</h1>
+
+            <div class="flex items-center justify-center pb-20 ">
+    
+                    <div class="bg-white rounded-2xl border shadow-xl p-10 max-w-lg pb-10 ">
+                        <div class="mt-10 -translate-y-1/2 transform">
+                            <img src="/img/verify.png" alt="verify" title="verify" class="mx-auto" />
+                        </div>
+
+                        <div class="flex flex-col items-center space-y-4">
+                            <h1 class="font-bold text-2xl text-gray-700 w-4/6 text-center">Email Address Confirmed</h1>
+                            <p class="text-sm text-gray-500 text-center w-5/6">Please check the message sent to kolagk@gmail.com. Confirming the message allow you to complete the process.</p>
+                            <a href="##" class="w-full sm:w-auto bg-indigo-500 text-gray-200 shadow-inner rounded hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                                    <div class="font-sans text-sm font-semibold px-10">Login</div>
+                            </a>
+
+                        </div>
+                    </div>
+            </div>            
+        </div>
+
+  
+        <hr class="my-60 max-w-lg my-6 rounded-lg border-b-2 border-violet-500 mx-auto" />
+
+        
+        <MainFooter />
+    </div>
+  )
+}
