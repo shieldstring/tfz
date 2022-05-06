3d19fb53b9d834d66e3de307fbc141b890503341
shieldstring <edmunddaniels06@gmail.com>
2022-05-06 13:25:11 +0100

    completed main bread crumb

--- src/components/MainBreadCrumb.js
+++ src/components/MainBreadCrumb.js
@@ -3,19 +3,19 @@ import React from 'react'
 export default function MainBreadCrumb() {
   return (
     <div class="bg-violet-500 pt-20 lg:pt-[20px] md:pt-[5px] sm:pt-[2px] justify-between ">     
-        <div class="flex z-[100] ">
-            <div class="ml-24 lg:w-1/2 mt-16  mb-12">
+        <div class="flex z-0 ">
+            <div class="ml-24 lg:w-1/2 mt-16  mb-12 justify-end">
                 <h2 class="text-gray-100 font-bold text-5xl tracking-tighter leading-tight" style={{fontFamily: "Roboto"}}>Tefzon Fantasy
                 </h2>                    
             </div>
-            <div class="lg:w-1/2 justify-end  ">
+            <div class=" lg:w-1/2 justify-end ">
                 <div class="mt-3 ml-20">
                     <img src="/img/Header Image.png"  alt='soccer' class="relative"  />
                 </div>
             </div>
         </div>
 
-        <div class="flex justify-between w-2/3 ml-24 ">
+        <div class="absolute flex justify-between w-2/3 ml-24 z-50 -mt-12">
             <div 
                 class="w-full block flex-grow md:flex md:justify-between md:w-auto">
                 <div>                    
@@ -58,6 +58,7 @@ export default function MainBreadCrumb() {
                     <a href="##"
                         class=" block md:inline-block text-sm px-4 py-4 leading-none rounded bg-green-500 text-gray-200 hover:text-gray-700 font-semibold hover:bg-gray-100 mt-4 md:mt-0">Sign Out</a>
                 </div>
+                
             </div>
         </div>
 
