ab88d184af1a2fe263fd0230f33886becde257ed
shieldstring <edmunddaniels06@gmail.com>
2022-05-28 09:31:18 +0100

    added loadingbox component

---
+++ src/components/LoadingBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LoadingBox() {
+    return (
+        <div className='block text-[#5B3CBF]/50 '>
+            <i className="fa fa-spinner fa-spin"></i> Loading...
+        </div>
+    )
+}
+
+export default LoadingBox;
